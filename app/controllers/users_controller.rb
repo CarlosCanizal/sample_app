3dc3a354e215905c0321c083063e61b7f9b8aaa4
Carlos Canizal <carlos@codecery.com>
2012-09-05 14:07:21 -0500

    End chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,13 +17,14 @@ class UsersController < ApplicationController
   	end
   end
 
-  def create 
-  	@user = User.new(params[:user])
-  	if @user.save
-  		flash[:success] = "Welcome to the Sample App!"
-  		redirect_to @user
-  	else
-  		 render 'new'
-  	end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
